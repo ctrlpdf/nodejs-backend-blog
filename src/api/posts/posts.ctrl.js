fbed37dba38f80b57785f43f4daf9907f8d6ed0b
ctrlpdf <ctrlpdf@gmail.com>
2023-06-20 14:01:57 +0900

    Added esm, mongodb

--- src/api/posts/posts.ctrl.js
+++ src/api/posts/posts.ctrl.js
@@ -1,102 +1,24 @@
-let postId = 1;
+import Post from '../../models/post';
 
-const posts = [
-  {
-    id: 1,
-    title: 'title',
-    body: 'body',
-  },
-];
-
-/**
- * POST /api/posts
- * { title, body }
- */
-exports.write = (ctx) => {
-  const { title, body } = ctx.request.body;
-  postId += 1;
-  const post = { id: postId, title, body };
-  posts.push(post);
-  ctx.body = post;
-};
-
-exports.list = (ctx) => {
-  ctx.body = posts;
-};
-
-/**
- * GET /api/posts/:id
- */
-exports.read = (ctx) => {
-  const { id } = ctx.params;
-  const post = posts.find((p) => p.id.toString() === id);
-  if (!post) {
-    ctx.status = 400;
-    ctx.body = {
-      message: 'There is no post',
-    };
-    return;
+export const write = async (ctx) => {
+  const { title, body, tags } = ctx.request.body;
+  const post = new Post({
+    title,
+    body,
+    tags,
+  });
+  try {
+    await post.save();
+    ctx.body = post;
+  } catch (e) {
+    ctx.throw(500, e);
   }
-  ctx.body = post;
 };
 
-/**
- * DELETE /api/posts/:id
- */
-exports.remove = (ctx) => {
-  const { id } = ctx.params;
-  const index = posts.findIndex((p) => p.id.toString() === id);
-  if (index === -1) {
-    ctx.status = 404;
-    ctx.body = {
-      message: 'There is no post',
-    };
-    return;
-  }
-  posts.splice(index, 1);
-  ctx.status = 204; // No Content
-};
+export const list = (ctx) => {};
 
-/**
- * PUT /api/posts/:id
- * { title, body }
- */
-exports.replace = (ctx) => {
-  const { id } = ctx.params;
-  const index = posts.findIndex((p) => p.id.toString() === id);
-  if (index === -1) {
-    ctx.status = 404;
-    ctx.body = {
-      message: 'There is no post',
-    };
-    return;
-  }
+export const read = (ctx) => {};
 
-  posts[index] = {
-    id,
-    ...ctx.request.body,
-  };
-  ctx.body = posts[index];
-};
+export const remove = (ctx) => {};
 
-/**
- * PATCH /api/posts/:id
- * { title, body }
- */
-exports.update = (ctx) => {
-  const { id } = ctx.params;
-  const index = posts.findIndex((p) => p.id.toString() === id);
-  if (index === -1) {
-    ctx.status = 404;
-    ctx.body = {
-      message: 'There is no post',
-    };
-    return;
-  }
-
-  posts[index] = {
-    ...posts[index],
-    ...ctx.request.body,
-  };
-  ctx.body = posts[index];
-};
+export const update = (ctx) => {};
